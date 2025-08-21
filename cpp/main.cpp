19370094e8f640af3c51911b5b1758d338a5e4f8
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-08-21 11:12:06 +0900

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,57 +3,34 @@
 
 using namespace std;
 
-int hangle;
-//0 : 한글
-//1 : 영어
-
-
-class LycosCralwer {
+//Google에서 제공하는 코드로 변경 불가
+class GoogleAPI { 
 public:
-	std::string run() {
-		// (생략)크롤링 후 데이터 반환 소스코드...
-		// 변경이 잦은 코드
-		return "lycos data...";
-	}
-};
-
-class YahooCralwer {
-public:
-	std::string run() {
-		// (생략)크롤링 후 데이터 반환 소스코드...
-		// 변경이 잦은 코드
-		return "yahoo data...";
-	}
-};
-
-class GoogleAPI {
-public:
-	std::string api() {
-		// (생략)API 호출 후 반환 소스코드...
+	std::string run(string context) {
+		// (생략) API 호출 후 반환 소스코드...
 		return "google data...";
 	}
 };
 
-class DBConnector {
+//Bing에서 제공하는 코드로 변경 불가
+class BingAPI {
 public:
-	void dbInit() {
-		// (생략)db 연결 코드...
-	}
-
-	void saveQuery(string site, string data) {
-		// (생략)db에 문자열 저장...
-	}
-
-	string loadQuery(string context) {
-		// (생략)db에 관련 문자열 가져오기
-		return "result";
-	}
-
-	void dbDisconnect() {
-		// (생략)db 접속종료...
+	std::string go(string context) {
+		// (생략) API 호출 후 반환 소스코드...
+		return "bing data...";
 	}
 };
 
+//Yahoo에서 제공하는 코드로 변경 불가
+class YahooAPI {
+public:
+	std::string api(string context) {
+		// (생략) API 호출 후 반환 소스코드...
+		return "yahoo data...";
+	}
+};
+
+//추론 Library이며 코드 변경 불가
 class Inferencer {
 public:
 	string inference(string modelName, string context) {
@@ -63,6 +40,11 @@ public:
 };
 
 int main() {
+
+	int hangle; //0 : 한글, 1 : 영어
+
+	string origin_input;
+	int origin_cnt;
 
 	cout << "무엇이든지 물어보세요 : ";
 	char buf[256];
@@ -74,7 +56,7 @@ int main() {
 	int flag = 0;
 	int cnt = 0;
 	for (int i = 0; i < line.size(); i++) {
-		if (line[i] > 127) {
+		if (line[i] > 127 || line[i] < 0) {
 			flag = 1;
 		}
 
@@ -82,50 +64,52 @@ int main() {
 		cnt++;
 	}
 
-	price = cnt * 100;
+	origin_input = line;
+
+
+	origin_cnt = cnt;
 
 	if (flag == 1) hangle = 0;
 	else hangle = 1;
 
 	cout << "\n사용할 검색엔진의 번호를 입력하세요.\n";
-	cout << "1. Lycos\n";
-	cout << "2. Yahoo\n";
-	cout << "3. Google\n";
+	cout << "1. Google\n";
+	cout << "2. Bing\n";
+	cout << "3. Yahoo\n";
 
 	cout << "================\n입력 : ";
 	int input1;
 	cin >> input1;
 	cout << "\n";
 
-	if (input1 == 1) price += 1000;
-	else if (input1 == 2) price += 1000;
-	else if (input1 == 3) price += 300;
+	if (input1 == 1) price += (30 * origin_cnt);
+	else if (input1 == 2) price += (20 * origin_cnt);
+	else if (input1 == 3) price += (15 * origin_cnt);
 
-	//1. 크롤링 해야하는 검색엔진
-	LycosCralwer t1;
-	YahooCralwer t2;
+	if (input1 == 1) {
+		if (origin_cnt < 10) {
+			price = 50 * origin_cnt;
+		}
+	}
+	else if (input1 == 2) {
+		if (origin_cnt <= 30) {
+			price /= 2;
+		}
+	}
 
 	//2. 검색엔진 API
-	GoogleAPI t3;
+	GoogleAPI t1;
+	BingAPI t2;
+	YahooAPI t3;
 
-	string r1 = t1.run();
-	string r2 = t2.run();
-	string r3 = t3.api();
+	string r1 = t1.run(origin_input);
+	string r2 = t2.go(origin_input);
+	string r3 = t3.api(origin_input);
 
 	string result = "";
 	if (input1 == 1) result = r1;
 	else if (input1 == 2) result = r2;
 	else if (input1 == 3) result = r3;
-
-	DBConnector db;
-
-	db.dbInit();
-
-	if (input1 == 1) db.saveQuery("Lycos", r1);
-	else if (input1 == 2) db.saveQuery("Yahoo", r2);
-	else if (input1 == 3) db.saveQuery("Google", r3);
-
-	db.dbDisconnect();
 
 	cout << "사용할 모델의 번호를 입력하세요.\n";
 
@@ -142,36 +126,58 @@ int main() {
 	int input;
 	cin >> input;
 	cout << "\n";
+
+	cnt = origin_cnt;
 	if (hangle == 0) {
-		if (input == 1) price += 500;
-		if (input == 2) price += 400;
-		if (input == 3) price += 100;
+		if (input == 1) {
+			if (cnt > 20) {
+				price += 1000;
+				price += (cnt * 50);
+			}
+			else {
+				price += 1000;
+				price += (cnt * 100);
+			}
+		}
+		if (input == 2) {
+			if (cnt >= 100) {
+				price += 100;
+				price += (cnt * 120);
+			}
+			else {
+				price += 100;
+				price += (cnt * 150);
+			}
+		}
+
+		if (input == 3) {
+			price += cnt * 130;
+		}
 	}
 	else {
-		if (input == 1) price += 100;
+		if (input == 1) {
+			price += cnt * 90;
+		}
 	}
 
 	Inferencer model;
 
-	//DB에서 관련 검색어 가져오기
-	string db1 = db.loadQuery(line);
-
 	//관련 검색어와 함께 모델에 추론시키기
 	string result2;
 	if (hangle == 0 && input == 1) {
-		result2 = model.inference("GPT", db1 + " " + line);
+		result2 = model.inference("GPT", result + " " + line);
 	}
 	else if (hangle == 0 && input == 2) {
-		result2 = model.inference("Gemini", db1 + " " + line);
+		result2 = model.inference("Gemini", result + " " + line);
 	}
 
 	else if (hangle == 0 && input == 3) {
-		result2 = model.inference("Llama", db1 + " " + line);
+		result2 = model.inference("Llama", result + " " + line);
 	}
 
 
 	else if (hangle == 1 && input == 1) {
-		result2 = model.inference("Llama", db1 + " " + line);
+		result2 = model.inference("Llama", result + " " + line);
 	}
 	else {
 		std::cout << "ERROR :: 유효하지 않은 입력입니다.\n";
@@ -180,6 +186,7 @@ int main() {
 
 	if (input1 < 1 || input1 > 3) {
 		std::cout << "ERROR :: 존재하지 않는 검색엔진입니다.\n";
+		return 0;
 	}
 
 	cout << "추론결과\n==============================\n";
